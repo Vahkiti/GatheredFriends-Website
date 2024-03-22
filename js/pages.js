bb16969f6bfdc06fcc01873b011397bc64a504de
Liam <36716882+Vahkiti@users.noreply.github.com>
2024-03-22 16:28:04 -0400

    WIP page handling

--- js/pages.js
+++ js/pages.js
@@ -1,27 +1,73 @@
-var landing;
-var home;
-var about;
-var watch;
-var read;
-var contact;
+var CurrentPage;
+var PrevPage;
 
 //URL Detection
-if (window.location.hash == "") {Default();} 
-else if (window.location.hash == "#Landing") {Landing();}
-else if (window.location.hash == "#Home") {Home();} 
-else if (window.location.hash == "#About") {About();} 
-else if (window.location.hash == "#Watch") {Watch();} 
-else if (window.location.hash == "#Read") {Read();} 
-else if (window.location.hash == "#Contact") {Contact();};
+if (window.location.hash == "") {
+	CurrentPage = 'splash';
+	HidePage();
+	PrevPage = CurrentPage;
+	document.getElementById(CurrentPage).style.display = "block";
+	document.getElementById(CurrentPage).style.zIndex = "auto";
+	window.location = (""+window.location).replace(/#[A-Za-z0-9_]*$/,'')+"#landing"
+} 
+else if (window.location.hash == "#landing") {
+	CurrentPage = 'splash';
+	HidePage();
+	PrevPage = CurrentPage;
+	document.getElementById(CurrentPage).style.display = "block";
+	document.getElementById(CurrentPage).style.zIndex = "auto";	
+}
+else if (window.location.hash == "#Home") {
+	CurrentPage = 'homepage';
+	HidePage();
+	ShowHeader();
+	PrevPage = CurrentPage;
+	document.getElementById(CurrentPage).style.display = "block";
+	document.getElementById(CurrentPage).style.zIndex = "auto";
+
+} 
+else if (window.location.hash == "#About") {
+	
+} 
+else if (window.location.hash == "#Watch") {
+	
+} 
+else if (window.location.hash == "#Read") {
+	
+} 
+else if (window.location.hash == "#Contact") {
+	
+};
 
 // Nav Buttons
-document.getElementById('landing').onclick = function () {Landing(); window.location = (""+window.location).replace(/#[A-Za-z0-9_]*$/,'')+"#Landing"}
-document.getElementById('enter').onclick = function () {Home();}
-document.getElementById('home').onclick = function () {Home();}
-document.getElementById('about').onclick = function () {About();}
-document.getElementById('watch').onclick = function () {Watch();}
-document.getElementById('read').onclick = function () {Read();}
-document.getElementById('contact').onclick = function () {Contact();}
+document.getElementById('landing').onclick = function () {
+	CurrentPage = 'splash';
+	FadePage(); 
+	HideHeader(); 
+	window.location = (""+window.location).replace(/#[A-Za-z0-9_]*$/,'')+"#landing"
+}
+document.getElementById('enter').onclick = function () {
+	CurrentPage = 'homepage';
+	FadePage(); 
+	ShowHeader();
+}
+document.getElementById('home').onclick = function () {
+	CurrentPage = 'homepage'; 
+	FadePage(); 
+	ShowHeader();
+}
+document.getElementById('about').onclick = function () {
+	
+}
+document.getElementById('watch').onclick = function () {
+	
+}
+document.getElementById('read').onclick = function () {
+	
+}
+document.getElementById('contact').onclick = function () {
+	
+}
 
 //Fade Functions
 function HideHeader() {
@@ -47,122 +93,32 @@ function ShrinkHeader() {
 	document.getElementById("header").style.maxHeight = "10vh";
 }
 
-function HidePages() {
-	if (landing == 0) {
-		document.getElementById("splash").style.opacity = "0";
-		document.getElementById("splash").style.zIndex = "-2";	
+function HidePage() {
+	document.getElementById('splash').style.display = "none";
+	document.getElementById('home').style.display = "none";
+}
+
+function FadePage() {
+	setTimeout(function () {
+		console.log("Current Page: " + CurrentPage);
+		console.log("Previous Page: " + PrevPage); 
+		
+		//Fade Out
+		document.getElementById(PrevPage).style.opacity = "0";
+		document.getElementById(PrevPage).style.zIndex = "-2";	
 		setTimeout(function () {
-			document.getElementById("splash").style.display = "none";
+			document.getElementById(PrevPage).style.display = "none";
+			PrevPage = CurrentPage;
 		}, 500);
-	}
-	else if (home == 0) {
 		
-	}
-	else if (about == 0) {
-		
-	}
-	else if (watch == 0) {
-		
-	}
-	else if (read == 0) {
-		
-	}
-	else if (contact == 0) {
-		
-	}
-}
-
-//Page Contents
-function Default() {
-	landing = 1;
-	home = 0;
-	about = 0;
-	watch = 0;
-	read = 0;
-	contact = 0;
-	HideHeader();
-	
-	document.getElementById("splash").style.display = "block";
-	document.getElementById("splash").style.zIndex = "auto";
-	window.location = (""+window.location).replace(/#[A-Za-z0-9_]*$/,'')+"#Landing"
-}	
-
-function Landing() {
-	landing = 1;
-	home = 0;
-	about = 0;
-	watch = 0;
-	read = 0;
-	contact = 0;	
-	HideHeader();
-	HidePages();
-	
-	document.getElementById("splash").style.display = "block";
-	window.location = (""+window.location).replace(/#[A-Za-z0-9_]*$/,'')+"#Landing"	
-	setTimeout(function () {
-		document.getElementById("splash").style.opacity = "1";
-		document.getElementById("splash").style.zIndex = "-2";
-	}, 1);
-	setTimeout(function () {
-		document.getElementById("splash").style.zIndex = "auto";
-	}, 550);
-}
-
-function Home() {
-	landing = 0;
-	home = 1;
-	about = 0;
-	watch = 0;
-	read = 0;
-	contact = 0;	
-	ShowHeader();
-	HidePages();
-}
-
-function About() {
-	landing = 0;
-	home = 0;
-	about = 1;
-	watch = 0;
-	read = 0;
-	contact = 0;	
-	ShowHeader();
-	ShrinkHeader();
-	HidePages();
-}
-
-function Watch() {
-	landing = 0;
-	home = 0;
-	about = 0;
-	watch = 1;
-	read = 0;
-	contact = 0;	
-	ShowHeader();
-	ShrinkHeader();
-	HidePages();
-}
-
-function Read() {
-	landing = 0;
-	home = 0;
-	about = 0;
-	watch = 0;
-	read = 1;
-	contact = 0;
-	ShowHeader();
-	ShrinkHeader();
-	HidePages();
-}
-
-function Contact() {
-	landing = 0;
-	home = 0;
-	about = 0;
-	watch = 0;
-	read = 0;
-	contact = 1;
-	ShowHeader();
-	ShrinkHeader();
-	HidePages();
+		//Fade In
+		document.getElementById(CurrentPage).style.display = "block";
+		setTimeout(function () {
+			document.getElementById(CurrentPage).style.opacity = "1";
+			document.getElementById(CurrentPage).style.zIndex = "-2";
+		}, 1);
+		setTimeout(function () {
+			document.getElementById(CurrentPage).style.zIndex = "auto";
+		}, 550);
+	}, 10);
 }
